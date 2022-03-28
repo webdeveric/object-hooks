e0e23d54de678a07f5ac6a623cc204bba86991d2
Eric King <eric@webdeveric.com>
2022-03-28 07:50:01 -0500

    Updated dependencies

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,5 @@
+import { toPascalCase } from '@webdeveric/utils';
+
 import {
   AFTER_PROPERTY,
   BEFORE_PROPERTY,
@@ -54,43 +56,6 @@ export async function getFirstValueAsync(...callbacks) {
       return value;
     }
   }
-}
-
-export function toPascalCase(text, customWords) {
-  let words = String(text).match( /[A-Z][a-z']+|\d+|[a-z']+/g );
-
-  if ( ! words ) {
-    return '';
-  }
-
-  // Uppercase the first letter of each word and replace apostrophe.
-  words = words.map( w =>  w.substr(0, 1).toUpperCase() + w.substr(1).replace('\'', '') );
-
-  if ( isObject( customWords ) ) {
-    const replacements = Object.entries(customWords).map(([ key, value ]) => [
-      key.toLowerCase(),
-      value,
-    ]);
-
-    if (replacements.length) {
-      // This takes a single word and returns the first matching replacement, if any.
-      const replaceCustomWords = word => {
-        const lowerWord = word.toLowerCase();
-
-        for (const [ key, value ] of replacements) {
-          if (key === lowerWord) {
-            return value;
-          }
-        }
-
-        return word;
-      };
-
-      words = words.map(replaceCustomWords);
-    }
-  }
-
-  return words.join('');
 }
 
 export function getHook( hooks, hookName ) {
